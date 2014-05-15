63b3e2aae6255481b6c6be719f49fcc1f2a2319e
eselinger <elizabeth.selinger@uoit.net>
2014-05-15 13:07:35 -0400

    shifting Direct coordinates

--- CHGCAR_pbc.py
+++ CHGCAR_pbc.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 
 import numpy as np
+import copy as cp
 
 #Read CHGCAR file
 infile = open('CHGCAR','r')
 f = open('out','w')
 
-#write header out
-sheader = infile.readline() + infile.readline()
-
-f.write(sheader)
+#copy supercell header to outfile
+header = infile.readline() + infile.readline()
+f.write(header)
 
 #multiply supercell and write out
 supercell = []
@@ -21,13 +21,50 @@ for i in xrange(3):
         f.write(str(element) + ' ')
     f.write('\n')
 
-#multiply number of atoms by 8
+#multiply number of atoms
 line = infile.readline()
-atoms = [8*int(element) for element in line.split()]
-for element in atoms:
-    f.write(str(element))
+anum = []
+for element in line.split(): anum.append(int(element)) #storing number of each atom
+
+atoms = [int(element)*8 for element in line.split()]
+for element in atoms: f.write(str(element) + '\n')
+
+#copy Direct header to outfile
+header = infile.readline()
+f.write(header)
 
 #Add molecule coordinates in each direction
+if len(anum) > 1:			#get total number of atoms
+    for i in xrange(len(anum)):
+        atot = anum[i] * anum[i+1]
+else:
+    atot = anum[0]
+
+coord = []
+coordinates = []
+ncoord = []
+newcoord = []
+
+for i in xrange(atot):			#read in atom coordinates
+    line = infile.readline()
+    coord = [float(element)*0.5 for element in line.split()] #original coords/2 for new supercell
+    ncoord = [float(element)+0.5 for element in coord] #make array of shifted coordinates
+    for element in coord:		#write out coords of original atoms for new supercell
+        f.write(str(element) + ' ')
+    f.write('\n')
+    coordinates.append(coord) #coords of original atoms for new supercell
+    newcoord.append(ncoord) #coords of complete shift (x,y,z)
+
+for j in xrange(0,3):
+    for i in xrange(0,atot):        
+        fcoord = cp.deepcopy(coordinates)
+        fcoord[i][j] = newcoord[i][j]   
+        for element in fcoord[i]:
+            f.write(str(element) + ' ')
+        f.write('\n')
+
+print coordinates
+print fcoord
 
 #Defined charges array
 CHG_i = infile.readlines()
